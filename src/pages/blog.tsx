9cc6f0fe0f5fc55a14a6ca5fcd1a032bca4712c8
Harman <5001432+hrgui@users.noreply.github.com>
2020-11-03 19:25:38 -0800

    fix(blog): sort the date correctly

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Paragraph from '../components/Paragraph';
-import BodyContainer from '../components/BodyContainer';
-import Layout from '../components/Layout'
-import styled from 'styled-components';
-import {BgHeader, Header, Title} from '../templates/blogTemplate';
-import {Link, useStaticQuery, graphql} from 'gatsby';
+import React from "react"
+import Paragraph from "../components/Paragraph"
+import BodyContainer from "../components/BodyContainer"
+import Layout from "../components/Layout"
+import styled from "styled-components"
+import { BgHeader, Header, Title } from "../templates/blogTemplate"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 const Styles = styled.div`
   max-width: 1170px;
@@ -12,15 +12,18 @@ const Styles = styled.div`
   margin-top: 64px;
   line-height: 1.6;
   padding-left: 16px;
-  padding-right:16px;
+  padding-right: 16px;
   font-size: 21px;
 
-  ${({theme}) => theme.breakpoints.down('sm')} {
+  ${({ theme }) => theme.breakpoints.down("sm")} {
     padding-left: 24px;
-    padding-right:24px;
+    padding-right: 24px;
   }
 
-  h2, h3, h4, h5 {
+  h2,
+  h3,
+  h4,
+  h5 {
     margin: 0;
     margin-top: 16px;
     margin-bottom: 16px;
@@ -32,18 +35,18 @@ const Styles = styled.div`
 
   h2 {
     font-size: 36px;
-    margin-bottom:0;
-    font-weight:300;
+    margin-bottom: 0;
+    font-weight: 300;
   }
 
-  h4 { 
+  h4 {
     margin-top: 0;
   }
 
   p {
     font-size: 21px;
 
-    ${({theme}) => theme.breakpoints.down('sm')} {
+    ${({ theme }) => theme.breakpoints.down("sm")} {
       font-size: 16px;
     }
   }
@@ -53,52 +56,68 @@ const Styles = styled.div`
     margin-top: 32px;
   }
 
-  a:link, a:visited {
+  a:link,
+  a:visited {
     color: #cc0000;
   }
-`;
+`
 
 const BlogIndexPage = () => {
-
-  const {blogData} = useStaticQuery(graphql`
+  const { blogData } = useStaticQuery(graphql`
     {
-    blogData: allMarkdownRemark(filter: {frontmatter: {path: {regex: "/blog/"}}}, sort: {fields: frontmatter___date, order: DESC}) {
-      edges {
-        node {
-          frontmatter {
-            title
-            excerpt
-            path
-            date
+      blogData: allMarkdownRemark(
+        filter: { frontmatter: { path: { regex: "/blog/" } } }
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              excerpt
+              path
+              date
+            }
           }
         }
       }
     }
-  }
-  `);
+  `)
 
-  let blogArticles = blogData.edges.map(x => x.node.frontmatter);
+  //TODO: since frontmatter doesn't sort by date correctly
+  // we will sort it this way - we will figure out to make frontmatter express dates
+  // later
+  blogData.edges.sort(
+    (a, b) =>
+      new Date(b.node.frontmatter.date).getTime() -
+      new Date(a.node.frontmatter.date).getTime()
+  )
 
-  return <Layout isBreakout>
-    <BodyContainer flexCenter>
-      <BgHeader />
-      <Header>
-        <Title>Blog</Title>
-      </Header>
-      <Styles>
-        {blogArticles.map(ba => <div key={ba.path} className="blogExcerpt">
-          <Link className="linkTitle" to={ba.path}><h2>{ba.title}</h2></Link>
-          <h4>{ba.date}</h4>
-          <p>
-            {ba.excerpt}
-          </p>
-          <div>
-            <Link to={ba.path}>Read more...</Link>
-          </div>
-        </div>)}
-      </Styles>
-    </BodyContainer>
-  </Layout>
-};
+  let blogArticles = blogData.edges.map(x => x.node.frontmatter)
 
-export default BlogIndexPage;
+  return (
+    <Layout isBreakout>
+      <BodyContainer flexCenter>
+        <BgHeader />
+        <Header>
+          <Title>Blog</Title>
+        </Header>
+        <Styles>
+          {blogArticles.map(ba => (
+            <div key={ba.path} className="blogExcerpt">
+              <Link className="linkTitle" to={ba.path}>
+                <h2>{ba.title}</h2>
+              </Link>
+              <h4>{ba.date}</h4>
+              <p>{ba.excerpt}</p>
+              <div>
+                <Link to={ba.path}>Read more...</Link>
+              </div>
+            </div>
+          ))}
+        </Styles>
+      </BodyContainer>
+    </Layout>
+  )
+}
+
+export default BlogIndexPage
