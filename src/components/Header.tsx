1b4625751e0984942e7fdb42840d03b2f5257a53
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-10-31 16:16:01 -0700

    feat: Drawer component is now done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,12 @@ const NavLink = ({
   href,
   children,
   exact,
+  onClick,
 }: {
   href: string
   children: string
   exact?: boolean
+  onClick?: () => void
 }) => {
   const router = useRouter()
 
@@ -20,13 +22,14 @@ const NavLink = ({
     <Link href={href}>
       <a
         className={classNames(
-          "text-gray-700 hover:text-gray-900 font-medium flex justify-center items-center pl-6 pr-6 h-16 hover:bg-red-700 hover:bg-opacity-5",
+          "text-gray-700 hover:text-gray-900 font-medium flex sm:justify-center items-center pl-6 pr-6 h-16 hover:bg-red-700 hover:bg-opacity-5",
           {
-            "text-red-700 hover:text-red-900 border-b-2 border-red-700": exact
+            "text-red-700 hover:text-red-900  bg-red-200 rounded-r-full sm:rounded-none sm:bg-transparent sm:border-b-2 border-red-700": exact
               ? router.pathname === href
               : router.pathname.includes(href),
           }
         )}
+        onClick={onClick}
       >
         {children}
       </a>
@@ -35,20 +38,53 @@ const NavLink = ({
 }
 
 const Header = () => {
-  const router = useRouter()
+  const [isOpen, setisOpen] = React.useState(false)
+  const handleSetIsOpen = () => setisOpen(!isOpen)
+
+  const links = (
+    <>
+      <NavLink onClick={handleSetIsOpen} href="/" exact>
+        Home
+      </NavLink>
+      <NavLink onClick={handleSetIsOpen} href="/posts">
+        Blog
+      </NavLink>
+      <NavLink onClick={handleSetIsOpen} href="/portfolio">
+        Portfolio
+      </NavLink>
+    </>
+  )
 
   return (
-    <div className="flex fixed items-center h-16 border-b border-gray-300 sm:justify-between w-full">
-      <Menu className="sm:hidden w-16" />
-      <Logo className="sm:pl-5" />
-      <nav className="hidden sm:flex h-16 justify-center items-center">
-        <NavLink href="/" exact>
-          Home
-        </NavLink>
-        <NavLink href="/posts">Blog</NavLink>
-        <NavLink href="/portfolio">Portfolio</NavLink>
-      </nav>
-    </div>
+    <>
+      <div className="flex fixed items-center h-16 border-b border-gray-300 sm:justify-between w-full">
+        <Menu onClick={handleSetIsOpen} className="sm:hidden w-16" />
+        <Logo className="sm:pl-5" />
+        <nav className="hidden sm:flex h-16 justify-center items-center">
+          {links}
+        </nav>
+      </div>
+      <div
+        onClick={handleSetIsOpen}
+        className={classNames("z-10 fixed inset-0 transition-opacity", {
+          hidden: !isOpen,
+        })}
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+      </div>
+      <aside
+        className={classNames(
+          "transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30",
+          {
+            "translate-x-0": isOpen,
+            "-translate-x-full": !isOpen,
+          }
+        )}
+      >
+        <Logo className="flex items-center pl-5 h-16 border-b-2 mb-2" />
+        {links}
+      </aside>
+    </>
   )
 }
 
--- src/components/icons/Menu.tsx
+++ src/components/icons/Menu.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import classnames from "classnames";
+import React from "react"
+import classnames from "classnames"
 
-interface Props {
-  className?: string;
+interface Props extends Omit<React.HTMLProps<SVGSVGElement>, "crossOrigin"> {
+  className?: string
 }
 
-const Menu = (props: Props) => {
+const Menu = ({ className, ...props }: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={classnames("h-6 w-6", props.className)}
+      className={classnames("h-6 w-6", className)}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
+      {...props}
     >
       <path
         strokeLinecap="round"
@@ -21,7 +22,7 @@ const Menu = (props: Props) => {
         d="M4 6h16M4 12h16M4 18h16"
       />
     </svg>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
