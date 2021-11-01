e70ead5ea5cf5f427f879ae895a5fb3ee261f631
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-11-01 00:18:01 -0700

    chore: remove unused deps

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-});
-
-module.exports = withMDX({
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-});
+}
