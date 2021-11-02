46f8249c390d1668c1236556007183132bb617df
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-11-01 21:29:43 -0700

    feat: usc.png was missing

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,9 +1,6 @@
 import React from "react"
-import Image from "next/image"
 
-interface Props {}
-
-const Education = (props: Props) => {
+const Education = () => {
   return (
     <div className="p-4 pt-8 pb-8 bg-blue-100 border-t-4 border-black">
       <h1 className="text-3xl font-medium mb-8">Education</h1>
@@ -11,7 +8,7 @@ const Education = (props: Props) => {
         <div className="w-28 pr-4">
           <img
             alt="University of Southern California"
-            src="https://www.harmanradix.com/static/usc-391742c924d04cd12285e3acca6da6d1.png"
+            src="/images/usc.png"
             className="w-20"
           />
         </div>
