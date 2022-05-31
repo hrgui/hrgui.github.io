e3bcfe46883c60fbf42ed573d426c5b2c7eb3515
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-05-30 19:58:03 -0700

    fix(Hero): hero spacing off in tablet

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import GithubProfileButton from "./GithubProfileButton"
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center items-start dark:text-gray-200 h-hero sm:h-hero-sm gplay-bg dark:dark-gplay-bg">
-      <div className="p-6 sm:p-0 flex flex-col justify-center items-start container mx-auto">
+      <div className="p-6 xl:p-0 flex flex-col justify-center items-start container mx-auto">
         <h1 className="text-4xl font-semibold leading-tight md:text-6xl md:leading-snug  xl:text-7xl xl:leading-snug 2xl:text-8xl 2xl:leading-snug">
           I ❤️ making{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-saltpan-400 to-malibu-500 dark:from-saltpan-500 dark:to-malibu-400">
