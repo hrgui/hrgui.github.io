7d0b0691f0d51b7ce7f7ccf9ac9d3de6d03104a9
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-02 20:41:28 -0700

    fix: fix weird drawer animation that starts in the beginning of the site

--- src/components/layout/Drawer.tsx
+++ src/components/layout/Drawer.tsx
@@ -9,7 +9,7 @@ const Drawer = ({ isOpen = false, children, className, ...props }: Props) => {
   return (
     <aside
       className={classNames(
-        "transform top-0 left-0 w-64 bg-white dark:bg-zinc-900 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30",
+        "sm:-translate-x-full transform top-0 left-0 w-64 bg-white dark:bg-zinc-900 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30",
         {
           "translate-x-0": isOpen,
           "-translate-x-full": !isOpen,
