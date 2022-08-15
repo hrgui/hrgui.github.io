32eb89aff0f8da79c0cdf7958a820d871fa853d2
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-14 21:55:15 -0700

    feat: slider properly wraps around now

---
+++ src/components/portfolio/Slider.test.tsx
@@ -0,0 +1,83 @@
+import { render, screen } from "@testing-library/preact";
+import { it, describe } from "vitest";
+import Slider from "./Slider";
+import userEvent from "@testing-library/user-event";
+
+it("should render normally without crashing - base case", () => {
+  render(
+    <Slider>
+      <div>
+        <a href="a.jpg" data-testid="a">
+          <img src="a.jpg" />
+        </a>
+      </div>
+      <div>
+        <a href="b.jpg" data-testid="b">
+          <img src="b.jpg" />
+        </a>
+      </div>
+      <div>
+        <a href="c.jpg" data-testid="c">
+          <img src="c.jpg" />
+        </a>
+      </div>
+      <div>
+        <a href="d.jpg" data-testid="d">
+          <img src="d.jpg" />
+        </a>
+      </div>
+    </Slider>
+  );
+  expect(screen.getByTestId("d")).toBeInTheDocument();
+  expect(screen.getByTestId("a")).toBeInTheDocument();
+  expect(screen.getByTestId("b")).toBeInTheDocument();
+  expect(screen.getByLabelText(`Navigate to Item 1`)).toBeInTheDocument();
+  expect(screen.getByLabelText(`Navigate to Item 4`)).toBeInTheDocument();
+});
+
+describe("navigation by buttons", () => {
+  it.skip("should be able to click to the next item, which doesn't wrap", async () => {
+    render(
+      <Slider>
+        <div>
+          <a href="a.jpg" data-testid="a">
+            <img src="a.jpg" />
+          </a>
+        </div>
+        <div>
+          <a href="b.jpg" data-testid="b">
+            <img src="b.jpg" />
+          </a>
+        </div>
+        <div>
+          <a href="c.jpg" data-testid="c">
+            <img src="c.jpg" />
+          </a>
+        </div>
+        <div>
+          <a href="d.jpg" data-testid="d">
+            <img src="d.jpg" />
+          </a>
+        </div>
+      </Slider>
+    );
+    expect(screen.getByTestId("d")).toBeInTheDocument();
+    expect(screen.getByTestId("a")).toBeInTheDocument();
+    expect(screen.getByTestId("b")).toBeInTheDocument();
+    expect(screen.getByLabelText(`Navigate to Item 1`)).toBeInTheDocument();
+
+    const itemToClick = screen.getByLabelText(`Navigate to Item 2`);
+
+    expect(itemToClick).toBeInTheDocument();
+
+    await userEvent.click(itemToClick);
+
+    expect(screen.getByTestId("a")).toBeInTheDocument();
+    expect(screen.getByTestId("b")).toBeInTheDocument();
+    expect(screen.getByTestId("c")).toBeInTheDocument();
+
+    screen.debug();
+
+    // expect(screen.getByLabelText(`Navigate to Item 4`)).toBeInTheDocument();
+  });
+});
