313df928c52f284c5e490905e29c6fb1fc8b2d57
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-25 13:33:13 -0700

    fix(NavDrawer): fix kbd a11y in desktop - it should be hidden there

--- src/components/nav/NavDrawer.tsx
+++ src/components/nav/NavDrawer.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const NavDrawer = ({ isOpen = false, links, onLinkClicked }: Props) => {
   return (
-    <Drawer isOpen={isOpen}>
+    <Drawer className="sm:hidden" isOpen={isOpen}>
       <Logo className="flex items-center pl-5 h-16 border-b-2 border-gray-700 mb-2" />
       {links.map((link) => cloneElement(link, { onClick: onLinkClicked }))}
     </Drawer>
