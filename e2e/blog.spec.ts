29429c86ab23ee6f1ba0313acc41e9fdda99b83c
Harman Goei <5001432+hrgui@users.noreply.github.com>
2023-09-03 17:12:12 -0700

    fix: await all the expects (#238)

--- e2e/blog.spec.ts
+++ e2e/blog.spec.ts
@@ -3,10 +3,9 @@ import { test, expect } from "@playwright/test";
 test("blog is present and all modules work", async ({ page }) => {
   await page.goto("/posts");
 
-  const header = page.getByRole("heading", { level: 1 });
-  expect(header).toBeInViewport();
-  expect(header).toContainText("Blog");
+  await expect(page.getByRole("heading", { level: 1 })).toBeVisible();
+  await expect(page.getByRole("heading", { level: 1 })).toContainText("Blog");
 
-  const firstBlogTitle = page.getByRole("heading", { level: 2 }).first();
+  const firstBlogTitle = await page.getByRole("heading", { level: 2 }).first();
   await firstBlogTitle.click();
 });
--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -3,21 +3,9 @@ import { test, expect } from "@playwright/test";
 test("home is present and all modules work", async ({ page }) => {
   await page.goto("/");
 
-  expect(page.getByTestId("section-hero")).toBeInViewport();
-
-  const section3SP = page.getByTestId("section-three-sell-points");
-  await section3SP.scrollIntoViewIfNeeded();
-  expect(section3SP).toBeInViewport();
-
-  const sectionTS = page.getByTestId("section-technical-skills");
-  await sectionTS.scrollIntoViewIfNeeded();
-  expect(sectionTS).toBeInViewport();
-
-  const sectionE = page.getByTestId("section-education");
-  await sectionE.scrollIntoViewIfNeeded();
-  expect(sectionE).toBeInViewport();
-
-  const sectionPortfolio = page.getByTestId("section-portfolio");
-  await sectionPortfolio.scrollIntoViewIfNeeded();
-  expect(sectionPortfolio).toBeInViewport();
+  await expect(page.getByTestId("section-hero")).toBeAttached();
+  await expect(page.getByTestId("section-three-sell-points")).toBeAttached();
+  await expect(page.getByTestId("section-technical-skills")).toBeAttached();
+  await expect(page.getByTestId("section-education")).toBeAttached();
+  await expect(page.getByTestId("section-portfolio")).toBeAttached();
 });
